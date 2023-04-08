7d28843f951653d98ee87ce6214a5ebae0d8fe3c
Andrei Jakab <drei222@mailbox.org>
2023-04-08 07:56:57 -0400

    updated script.js per 'Values & Variables' lecture

--- 01-Fundamentals-Part-1/section_code/script.js
+++ 01-Fundamentals-Part-1/section_code/script.js
@@ -1,5 +1,17 @@
 let js = 'amazing';
-
-if (js === 'amazing') alert('JS is FUN!');
-
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let name = "Jonas";
+let $function = 27;
+let PI = 3.151692;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
