1286db9712ff8d003bb599585837e486e1fab34f
Andrei Jakab <drei222@mailbox.org>
2023-04-08 08:18:21 -0400

    committing my solution to assignment 1

---
+++ 01-Fundamentals-Part-1/assignment_01_values-variables/script.js
@@ -0,0 +1,7 @@
+// variable declaration
+let country = "Canada";
+let continent = "North America";
+let population = 38654702;
+
+// display variables
+console.log("I live in " + country + ", a country that has a population of " + population + " and which is located in " + continent + ".");
