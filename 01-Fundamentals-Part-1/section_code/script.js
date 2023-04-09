21aa92cb5c979b7fd12b9174b06cd17a0137842b
Andrei Jakab <drei222@mailbox.org>
2023-04-09 07:26:38 -0400

    12. Data Types: code from lecture

--- 01-Fundamentals-Part-1/section_code/script.js
+++ 01-Fundamentals-Part-1/section_code/script.js
@@ -1,3 +1,7 @@
+//
+// Lecture: Values & Variables
+//
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -9,9 +13,37 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// variable name conventions
 let name = "Jonas";
 let $function = 27;
 let PI = 3.151692;
 
 let myFirstJob = 'Programmer';
 let myCurrentJob = 'Teacher';
+*/
+
+//
+// Lecture: Data Types
+//
+
+console.log("true");
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2011;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
