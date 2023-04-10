9bb879844ea7c49d8e31400f2b33b6ea19754ccc
Andrei Jakab <drei222@mailbox.org>
2023-04-10 15:53:04 -0400

    13. let, const, and var: completed assignment

---
+++ 01-Fundamentals-Part-1/assignment_03_let-const-var/script.js
@@ -0,0 +1,20 @@
+// variable declaration
+const country = "Canada";
+const continent = "North America";
+let population = 38654702;
+
+// display variables
+console.log("I live in " + country + ", a country that has a population of " + population + " and which is located in " + continent + ".");
+
+// variable declaration
+const isIsland = false;
+const language = "English";
+
+// show data types
+console.log("Type of 'isIsland': " + typeof isIsland);
+console.log("Type of 'population': " + typeof population);
+console.log("Type of 'country': " + typeof country);
+console.log("Type of 'language': " + typeof language);
+
+// attempting to change a const variable
+country = "USA";
