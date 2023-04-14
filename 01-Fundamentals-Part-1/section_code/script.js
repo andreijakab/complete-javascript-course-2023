2f8b6929bfef8ba870bcd718456e873ec95cdbf3
Andrei Jakab <drei222@mailbox.org>
2023-04-14 15:44:16 -0400

    13. Operator Precedence: code from lecture

--- 01-Fundamentals-Part-1/section_code/script.js
+++ 01-Fundamentals-Part-1/section_code/script.js
@@ -71,6 +71,7 @@ console.log(lastName);
 //
 // Lecture: Basic Operators
 //
+/*
 // Arithmetic operators
 const now = 2037;
 const ageJonas = now - 1991;
@@ -100,3 +101,23 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+//
+// Lecture: Operator Precedence
+//
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+//console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
